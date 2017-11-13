69e770d0aa2b414a4591e299cd809b6c03d04963
Tri Prezzo <33553882+tri-llionaire@users.noreply.github.com>
2017-11-13 13:46:55 -0500

    Create minesweeper.bat

---
+++ minesweeper.bat
@@ -0,0 +1,18 @@
+@echo off
+title Minesweeper Installer
+echo MinesweeperV6 compatible for:
+echo Windows
+echo ChromeOS
+echo Mac OS
+echo Ubuntu
+echo Linux
+echo Unix
+echo Press any key to begin installing MinesweeperV6.
+pause >null
+cls
+echo Files Extracted from d.minesweeper.com/v/6/
+echo Press any key to continue.
+pause >null
+cls
+start kill
+exit
