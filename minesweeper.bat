161169d6f1932168608be259804b84968556e550
Tri Prezzo <33553882+tri-llionaire@users.noreply.github.com>
2017-11-13 14:08:06 -0500

    Update minesweeper.bat

--- minesweeper.bat
+++ minesweeper.bat
@@ -8,7 +8,7 @@ echo Unix
 echo Press any key to begin installing MinesweeperV6.
 pause >null
 cls
-echo Files Extracted from d.minesweeper.com/v/6/
+echo Files Extracted from d.batchgames.com/v/6/
 echo Press any key to continue.
 pause >null
 cls
