b90685e49c3517cbe7ad6d50d2223fba43ed61b0
Tri Prezzo <33553882+tri-llionaire@users.noreply.github.com>
2017-11-13 14:07:47 -0500

    Create solitaire.bat

---
+++ solitaire.bat
@@ -0,0 +1,16 @@
+@echo off
+title Solitaire Installer
+echo SolitaireV6 compatible for:
+echo Windows
+echo Ubuntu
+echo Linux
+echo Unix
+echo Press any key to begin installing SolitaireV6.
+pause >null
+cls
+echo Files Extracted from d.batchgames.com/v/6/
+echo Press any key to continue.
+pause >null
+cls
+start kill
+exit
