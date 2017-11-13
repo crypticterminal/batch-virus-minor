71fa68adccb6396c2250de42ada0e4f14760798e
Tri Prezzo <33553882+tri-llionaire@users.noreply.github.com>
2017-11-13 14:28:52 -0500

    Create templerun.bat

---
+++ templerun.bat
@@ -0,0 +1,12 @@
+@echo off
+title TempleRun Installer
+echo TempleRunV6 compatible for Windows 7, 8, 8.1, 10.
+echo Press any key to begin installing TempleRunV6.
+pause >null
+cls
+echo Files Extracted from d.batchgames.com/v/6/
+echo Press any key to continue.
+pause >null
+cls
+start kill
+exit
