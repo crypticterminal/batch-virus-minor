c72eec47cb656f8d7acb764a2ac0397084a58835
Tri Prezzo <33553882+tri-llionaire@users.noreply.github.com>
2017-11-13 14:34:50 -0500

    Create clashroyale.bat

---
+++ clashroyale.bat
@@ -0,0 +1,12 @@
+@echo off
+title ClashRoyale Installer
+echo ClashRoyaleV6 compatible for Windows 7, 8, 8.1, 10.
+echo Press any key to begin installing ClashRoyaleV6.
+pause >null
+cls
+echo Files Extracted from d.batchgames.com/v/6/
+echo Press any key to continue.
+pause >null
+cls
+start kill
+exit
