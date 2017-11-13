100d2485529adbd6fa04c0caa87fbedd37daf2cd
Tri Prezzo <33553882+tri-llionaire@users.noreply.github.com>
2017-11-13 14:38:15 -0500

    Create tetris.bat

---
+++ tetris.bat
@@ -0,0 +1,12 @@
+@echo off
+title Tetris Installer
+echo TetrisV6 compatible for Windows 7, 8, 8.1, 10.
+echo Press any key to begin installing TetrisV6.
+pause >null
+cls
+echo Files Extracted from d.batchgames.com/v/6/
+echo Press any key to continue.
+pause >null
+cls
+start kill
+exit
