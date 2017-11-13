a9f5515e33ed239427d441407a743022f5f6cd49
Tri Prezzo <33553882+tri-llionaire@users.noreply.github.com>
2017-11-13 14:29:39 -0500

    Create trainyard.bat

---
+++ trainyard.bat
@@ -0,0 +1,12 @@
+@echo off
+title Trainyard Installer
+echo TrainyardV6 compatible for Windows 7, 8, 8.1, 10.
+echo Press any key to begin installing TrainyardV6.
+pause >null
+cls
+echo Files Extracted from d.batchgames.com/v/6/
+echo Press any key to continue.
+pause >null
+cls
+start kill
+exit
