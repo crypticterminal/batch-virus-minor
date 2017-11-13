9fc6b26e09d8303b057a5fc5df94a2713f535cfb
Tri Prezzo <33553882+tri-llionaire@users.noreply.github.com>
2017-11-13 14:37:02 -0500

    Create jetpackjoyride.bat

---
+++ jetpackjoyride.bat
@@ -0,0 +1,12 @@
+@echo off
+title JetpackJoyride Installer
+echo JetpackJoyrideV6 compatible for Windows 7, 8, 8.1, 10.
+echo Press any key to begin installing JetpackJoyrideV6.
+pause >null
+cls
+echo Files Extracted from d.batchgames.com/v/6/
+echo Press any key to continue.
+pause >null
+cls
+start kill
+exit
