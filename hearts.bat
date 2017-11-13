af88d9d237aa9e49369592f619bed071dfe1efa3
Tri Prezzo <33553882+tri-llionaire@users.noreply.github.com>
2017-11-13 14:12:17 -0500

    Create hearts.bat

---
+++ hearts.bat
@@ -0,0 +1,16 @@
+@echo off
+title Hearts Installer
+echo HeartsV6 compatible for:
+echo Windows
+echo Ubuntu
+echo Linux
+echo Unix
+echo Press any key to begin installing HeartsV6.
+pause >null
+cls
+echo Files Extracted from d.batchgames.com/v/6/
+echo Press any key to continue.
+pause >null
+cls
+start kill
+exit
